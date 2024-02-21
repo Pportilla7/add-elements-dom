d467dff054b36637e00d0c94daa0a459daebb2dd
Pablo Portilla Rivero <pportilla@rfe.es>
2024-02-21 16:54:33 +0100

    Se resuelve el ejercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+let list=document.getElementById('lista');
+console.log(list);
+
+let boton=document.getElementById('agregar');
+console.log(boton);
+
+function agregarelemento(){
+    let elemento=document.createElement('li');
+    elemento.textContent=prompt('Introduce un elemente a la lista');
+    list.appendChild(elemento);
+}
+
+boton.addEventListener('click',agregarelemento);
+
